6e19530e114e536e2742478d9605cd164fd3a486
Kajalakshmy <Kajalakshmy96@gmail.com>
2020-06-17 00:00:14 +0530

    Update is completed

--- screens/ViewTaskScreen.js
+++ screens/ViewTaskScreen.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, StatusBar, SafeAreaView, ScrollView } from 'react-native';
+import { View, StyleSheet, StatusBar, SafeAreaView, ScrollView, Picker } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 //import { Card } from 'react-native-paper';
-import { Container, Header, Content, Card, CardItem, List, ListItem, Title, Text, Button, Body, Left, Right, Icon } from 'native-base';
-
+import { Input, CheckBox, Button } from 'react-native-elements';
+import { Container, Header, Content, Card, CardItem, List, ListItem, Title, Text, Body, Left, Right, Icon } from 'native-base';
 
 const ViewTaskScreen = (props) => {
 
@@ -11,6 +11,7 @@ const ViewTaskScreen = (props) => {
   const theme = useTheme();
 
   const [task, setTask] = useState([]);
+  const [taskState, setTaskState] = useState("N");
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -30,6 +31,7 @@ const ViewTaskScreen = (props) => {
       .then(response => {
         console.log(response);
         setTask(response.result);
+        setTaskState(response.result.state);
         setIsLoading(false);
       })
       .catch(error => alert(error));
@@ -44,11 +46,58 @@ const ViewTaskScreen = (props) => {
       return "Partially Completed";
     } else if (state == 'O') {
       return "Progress";
-    } else if (state == 'F') {
-      return "Failed";
+    } else if (state == 'E') {
+      return "Cancelled";
     }
   }
 
+  const updateState = (state) => {
+    fetch(`http://10.0.2.2:8163/api/v1/task/state/` + props.route.params.id,
+      {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGIwNjFhZjEwMWQxMmRhMWQxZjU3MGQ4MDYyOGVmOTE0NmFjYzYzOWY1Y2Q2ZmFiY2RiNGJlNGEzYzE1NDRhYzBkNzcyMzc4MTg2NjlkMjUiLCJpYXQiOjE1OTE5NDMxMDAsIm5iZiI6MTU5MTk0MzEwMCwiZXhwIjoxNjIzNDc5MTAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.yEWBza9x4SAsq_facmrvKq1-LDdHkW1YpH1KyXawXKiHoTE4-X3c9Tj29keKzmGRkvF3q4jM6ogDSC9wr1POa9MLCYBG-CMRWHRrBJ6fpl_MvpZxyIg2LTXY2-5p0LgAk-Clp1yPSg5ohaVKynJW_hs0WbmFwOnpPHPOzk_rc5yA3oXpXbqSj-jFESq1JyVPWZm7_P0O9gPz7jIVtV-FfJpnyuNULfYS-o8paK4W6DS02eNkxJtXlqmLsTgZi0f7CH0sF9xrk9h5Ssp5krWvB1ED6KYJuJ2UvJ7-zRFDQpHHgJnK5APs1lD8iH6Oc42TscbS1ddw_sxD0dIERW6h7BY1BwPTQjwThcYrpDRkdfXRQrASdj1_KGwrl8ZUf36x5K0_qs1Qm_B86pdQIc__Dh0dof5fZoenxGMzd8kaWMnzypA8jTe3HIoq0hkPgW8jlwBWyA771N1MoGrppYXjbS4H_aWU-KBm6Lc-7JN_0UPeujcxIBjl_QvHyAyf8Qc9ywPyZMM5VKwhxYLBPKyPNcP8ju7eLMBp-gmRvUakAVuNEnfSYMD5Y4FihT1ItswoJh37MXbtHj0mjPx-cFyEzkhYXcsjTGoFI5GEi9daF7sknQuYCKSY16NGU0XeS0ZJtbkmsykDrMe7YO-DKl69OYilFKe9AgNjdwnKldYgjmg'
+        },
+        body: JSON.stringify({
+          state: state
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        if (response.code == 200) {
+          setTaskState(state);
+          console.log("State successfully updated!");
+        }
+      })
+      .catch(error => alert(error));
+  }
+  
+  const deleteTask = () => {
+    fetch(`http://10.0.2.2:8163/api/v1/task/delete/` + props.route.params.id,
+      {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGIwNjFhZjEwMWQxMmRhMWQxZjU3MGQ4MDYyOGVmOTE0NmFjYzYzOWY1Y2Q2ZmFiY2RiNGJlNGEzYzE1NDRhYzBkNzcyMzc4MTg2NjlkMjUiLCJpYXQiOjE1OTE5NDMxMDAsIm5iZiI6MTU5MTk0MzEwMCwiZXhwIjoxNjIzNDc5MTAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.yEWBza9x4SAsq_facmrvKq1-LDdHkW1YpH1KyXawXKiHoTE4-X3c9Tj29keKzmGRkvF3q4jM6ogDSC9wr1POa9MLCYBG-CMRWHRrBJ6fpl_MvpZxyIg2LTXY2-5p0LgAk-Clp1yPSg5ohaVKynJW_hs0WbmFwOnpPHPOzk_rc5yA3oXpXbqSj-jFESq1JyVPWZm7_P0O9gPz7jIVtV-FfJpnyuNULfYS-o8paK4W6DS02eNkxJtXlqmLsTgZi0f7CH0sF9xrk9h5Ssp5krWvB1ED6KYJuJ2UvJ7-zRFDQpHHgJnK5APs1lD8iH6Oc42TscbS1ddw_sxD0dIERW6h7BY1BwPTQjwThcYrpDRkdfXRQrASdj1_KGwrl8ZUf36x5K0_qs1Qm_B86pdQIc__Dh0dof5fZoenxGMzd8kaWMnzypA8jTe3HIoq0hkPgW8jlwBWyA771N1MoGrppYXjbS4H_aWU-KBm6Lc-7JN_0UPeujcxIBjl_QvHyAyf8Qc9ywPyZMM5VKwhxYLBPKyPNcP8ju7eLMBp-gmRvUakAVuNEnfSYMD5Y4FihT1ItswoJh37MXbtHj0mjPx-cFyEzkhYXcsjTGoFI5GEi9daF7sknQuYCKSY16NGU0XeS0ZJtbkmsykDrMe7YO-DKl69OYilFKe9AgNjdwnKldYgjmg'
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        if (response.code == 200) {
+          //setTaskState(state);          
+          console.log("Task successfully deleted!");
+          props.navigation.navigate("Home");
+        }
+      })
+      .catch(error => alert(error));
+  }
   return (
     <SafeAreaView >
       <ScrollView >
@@ -68,10 +117,39 @@ const ViewTaskScreen = (props) => {
                 <Text>Reminder - {task.remind ? <Text>On</Text> : <Text>Off</Text>} </Text>
               </ListItem>
               <ListItem>
-                <Text>Status - {getTaskState(task.state)}</Text>
+                <Text>Status - {getTaskState(taskState)}</Text>
               </ListItem>
             </List>
-
+            <Picker
+              selectedValue={taskState}
+              onValueChange={(value) => updateState(value)}>
+              {/* <Picker.Item label="Select a Category" value="N"></Picker.Item> */}
+              <Picker.Item label="New" value="N"></Picker.Item>
+              <Picker.Item label="Completed" value="C"></Picker.Item>
+              <Picker.Item label="Progress" value="O"></Picker.Item>
+              <Picker.Item label="Cancelled" value="E"></Picker.Item>
+              <Picker.Item label="Partially" value="P"></Picker.Item>
+            </Picker>
+            <Button
+              title="Delete"
+              buttonStyle={{
+                backgroundColor: 'red',
+                width: '60%',
+                marginLeft: '20%',
+                marginBottom:10
+              }}
+            onPress={() => deleteTask()}
+            />
+            <Button
+              title="Update"
+              buttonStyle={{
+                backgroundColor: 'blue',
+                width: '60%',
+                marginLeft: '20%'
+              }}
+            onPress={() => props.navigation.navigate("UpdateTask", {id:props.route.params.id})}
+            
+            />
           </Content>
         </Container>
       </ScrollView>
@@ -87,4 +165,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  button: {
+
+  }
 });
