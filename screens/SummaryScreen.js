3dc25f39e1080b2c47aad97ef473e0e0533c0264
Kajalakshmy <Kajalakshmy96@gmail.com>
2020-06-19 02:53:40 +0530

    url and token updated

--- screens/SummaryScreen.js
+++ screens/SummaryScreen.js
@@ -23,13 +23,13 @@ const SummaryScreen = ({ navigation }) => {
 
   useEffect(() => {
     fetch(
-      `http://10.0.2.2:8163/api/v1/summary`,
+      `https://pure-tundra-14665.herokuapp.com/api/v1/summary`,
       {
         method: "POST",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGIwNjFhZjEwMWQxMmRhMWQxZjU3MGQ4MDYyOGVmOTE0NmFjYzYzOWY1Y2Q2ZmFiY2RiNGJlNGEzYzE1NDRhYzBkNzcyMzc4MTg2NjlkMjUiLCJpYXQiOjE1OTE5NDMxMDAsIm5iZiI6MTU5MTk0MzEwMCwiZXhwIjoxNjIzNDc5MTAwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.yEWBza9x4SAsq_facmrvKq1-LDdHkW1YpH1KyXawXKiHoTE4-X3c9Tj29keKzmGRkvF3q4jM6ogDSC9wr1POa9MLCYBG-CMRWHRrBJ6fpl_MvpZxyIg2LTXY2-5p0LgAk-Clp1yPSg5ohaVKynJW_hs0WbmFwOnpPHPOzk_rc5yA3oXpXbqSj-jFESq1JyVPWZm7_P0O9gPz7jIVtV-FfJpnyuNULfYS-o8paK4W6DS02eNkxJtXlqmLsTgZi0f7CH0sF9xrk9h5Ssp5krWvB1ED6KYJuJ2UvJ7-zRFDQpHHgJnK5APs1lD8iH6Oc42TscbS1ddw_sxD0dIERW6h7BY1BwPTQjwThcYrpDRkdfXRQrASdj1_KGwrl8ZUf36x5K0_qs1Qm_B86pdQIc__Dh0dof5fZoenxGMzd8kaWMnzypA8jTe3HIoq0hkPgW8jlwBWyA771N1MoGrppYXjbS4H_aWU-KBm6Lc-7JN_0UPeujcxIBjl_QvHyAyf8Qc9ywPyZMM5VKwhxYLBPKyPNcP8ju7eLMBp-gmRvUakAVuNEnfSYMD5Y4FihT1ItswoJh37MXbtHj0mjPx-cFyEzkhYXcsjTGoFI5GEi9daF7sknQuYCKSY16NGU0XeS0ZJtbkmsykDrMe7YO-DKl69OYilFKe9AgNjdwnKldYgjmg'
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGUxMGU2NTNmZGRhZWFiYmQ3YzU1MWQzODZjZWE5NGFkNzEwNjg3ZWZkOWI2ZjkxMjVhNTU1MzE4NmNlN2VlZDhhOGFjOGRjMzIzMDExNjciLCJpYXQiOjE1OTI1MTIyNTUsIm5iZiI6MTU5MjUxMjI1NSwiZXhwIjoxNjI0MDQ4MjU0LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.SOUjXHDjUNbM4FUKZbLMl5sC_N0rEC14e5zF-tkIzO3ZuEW9TZ4jZZYu4ZkBsXRJG_x7edtuzbwstYqSbx7dy9Tb4UcvdDFjwdqgmcoB1xOG3stlI4djTyEk6LlFvn7qeP3Pgz8i41dzGnCY-XvUvuXy_aazEwznGPyOzeLylSDOpcowljGk3rfaWMTHzPdwf0ZxHcjbaG_-WqAonY_PdsuGAu3lkojtrXyYrihyLxuy4ekDgndUS3dCxNFeAHJt_Uc8GebDC3HJdbsK1ON3Bvc_MNkAaEuGzn9UxD9sRXYtyUaV6T4udZUgEAzeFUfEMmPc_W1NBg19BYAoxu7miMxM8T8Ms_VB5YfNzYaSKuUkp3mUObtopXsGCvgAkk4mh8EAlG8Vav_ZCV1s_M1aVZ0fLeEIfbNx-TFvvyziEbQLvOEWiVlN4-hos5tO34fomAwxUqhStWttx-iVBGe5hVb8slqyOcEGf7rIYEEgGq_BtyLn0D3v0KwesShsjFtniiuG4qnO6T5K8ciV1aiF_5Eq-Ev5kKWZPVD2YzFFOoxMQWx15pwvHLEJJJVdU3JZR4noi9zRG8UEZ4XoXB6GYR-pH4uCP2S-M-x73031WkVq6IIVx7uFkfXV2C1E2Zz9WN_CJ0zEJMd4fAnPmq0KRsEkqRuJ4zKyqbwe-IAGwzM'
         },
         body: JSON.stringify({
           time_period: "M"
@@ -52,10 +52,10 @@ const SummaryScreen = ({ navigation }) => {
           console.log(summary);
           tmpPieChartData = [
             // { name: 'New Tasks', population: parseInt(summary.new), color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-            { name: 'Completed', population: parseInt(summary.completed), color: '#F00', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-            { name: 'Partially Completed', population: parseInt(summary.partially), color: 'red', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-            { name: 'Progress', population: parseInt(summary.progress), color: '#ffffff', legendFontColor: '#7F7F7F', legendFontSize: 15 },
-            { name: 'Failed', population: parseInt(summary.new), color: 'rgb(0, 0, 255)', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+            { name: 'Completed', population: parseInt(summary.completed), color: '#52d726', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Partially Completed', population: parseInt(summary.partially), color: '#e01e84', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Progress', population: parseInt(summary.progress), color: '#ff7300', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+            { name: 'Failed', population: parseInt(summary.new), color: '#ff0000', legendFontColor: '#7F7F7F', legendFontSize: 15 }
           ];
           console.log(tmpPieChartData);
         });
@@ -70,10 +70,10 @@ const SummaryScreen = ({ navigation }) => {
     <ScrollView
       key={Math.random()}
       style={{
-        backgroundColor: chartConfig.backgroundColor
+        // backgroundColor: chartConfig.backgroundColor
       }}
     >
-      <Text >Bezier Line Chart</Text>
+      {/* <Text >Tasks Summay</Text> */}
       <PieChart
         data={pieChartData}
         height={220}
