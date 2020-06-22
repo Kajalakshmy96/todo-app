62e79998dfdfdcd4dabb9fc00e7c123493a413af
Kajalakshmy <Kajalakshmy96@gmail.com>
2020-06-23 03:11:00 +0530

    Firebase is integrated

--- App.js
+++ App.js
@@ -7,18 +7,18 @@
  */
 
 import React, { useEffect } from 'react';
-import { View, ActivityIndicator } from 'react-native';
-import { 
-  NavigationContainer, 
+import { Alert, View, ActivityIndicator } from 'react-native';
+import {
+  NavigationContainer,
   DefaultTheme as NavigationDefaultTheme,
   DarkTheme as NavigationDarkTheme
 } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { 
-  Provider as PaperProvider, 
+import {
+  Provider as PaperProvider,
   DefaultTheme as PaperDefaultTheme,
-  DarkTheme as PaperDarkTheme, 
+  DarkTheme as PaperDarkTheme,
   configureFonts
 } from 'react-native-paper';
 
@@ -33,6 +33,8 @@ import RootStackScreen from './screens/RootStackScreen';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import Configs from './model/config';
+
+import firebase from "react-native-firebase";
 
 const Drawer = createDrawerNavigator();
 
@@ -58,7 +60,7 @@ const App = () => {
       text: '#333333'
     }
   }
-  
+
   const CustomDarkTheme = {
     ...NavigationDarkTheme,
     ...PaperDarkTheme,
@@ -73,28 +75,28 @@ const App = () => {
   const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
 
   const loginReducer = (prevState, action) => {
-    switch( action.type ) {
-      case 'RETRIEVE_TOKEN': 
+    switch (action.type) {
+      case 'RETRIEVE_TOKEN':
         return {
           ...prevState,
           userToken: action.token,
           isLoading: false,
         };
-      case 'LOGIN': 
+      case 'LOGIN':
         return {
           ...prevState,
           // userName: action.id,
           userToken: action.token,
           isLoading: false,
         };
-      case 'LOGOUT': 
+      case 'LOGOUT':
         return {
           ...prevState,
           // userName: null,
           userToken: null,
           isLoading: false,
         };
-      case 'REGISTER': 
+      case 'REGISTER':
         return {
           ...prevState,
           // userName: action.id,
@@ -107,28 +109,28 @@ const App = () => {
   const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
 
   const authContext = React.useMemo(() => ({
-    signIn: async(foundUser) => {
+    signIn: async (foundUser) => {
       // setUserToken('fgkj');
       // setIsLoading(false);
       const userToken = String(foundUser.access_token);
       // const userName = foundUser[0].username;
-      
+
       try {
         Configs.accessToken = userToken;
         await AsyncStorage.setItem('userToken', userToken);
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
       // console.log('user token: ', userToken);
       // dispatch({ type: 'LOGIN', id: userName, token: userToken });
       dispatch({ type: 'LOGIN', token: userToken });
     },
-    signOut: async() => {
+    signOut: async () => {
       // setUserToken(null);
       // setIsLoading(false);
       try {
         await AsyncStorage.removeItem('userToken');
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
       dispatch({ type: 'LOGOUT' });
@@ -138,18 +140,18 @@ const App = () => {
       // setIsLoading(false);
     },
     toggleTheme: () => {
-      setIsDarkTheme( isDarkTheme => !isDarkTheme );
+      setIsDarkTheme(isDarkTheme => !isDarkTheme);
     }
   }), []);
 
   useEffect(() => {
-    setTimeout(async() => {
+    setTimeout(async () => {
       // setIsLoading(false);
       let userToken;
       userToken = null;
       try {
         userToken = await AsyncStorage.getItem('userToken');
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
       // console.log('user token: ', userToken);
@@ -157,30 +159,174 @@ const App = () => {
     }, 1000);
   }, []);
 
-  if( loginState.isLoading ) {
-    return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size="large"/>
+
+  useEffect(() => {
+    // code to run on component mount
+    //we check if user has granted permission to receive push notifications.
+    checkPermission();
+    // Register all listener for notification 
+    createNotificationListeners();
+  }, [])
+
+
+  const checkPermission = async () => {
+    const enabled = await firebase.messaging().hasPermission();
+    // If Premission granted proceed towards token fetch
+    if (enabled) {
+      getToken();
+    } else {
+      // If permission hasn’t been granted to our app, request user in requestPermission method. 
+      requestPermission();
+    }
+  }
+  // async checkPermission() {
+  //   const enabled = await firebase.messaging().hasPermission();
+  //   // If Premission granted proceed towards token fetch
+  //   if (enabled) {
+  //     this.getToken();
+  //   } else {
+  //     // If permission hasn’t been granted to our app, request user in requestPermission method. 
+  //     this.requestPermission();
+  //   }
+  // }
+
+  const getToken = async () => {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  }
+  // async getToken() {
+  //   let fcmToken = await AsyncStorage.getItem('fcmToken');
+  //   if (!fcmToken) {
+  //     fcmToken = await firebase.messaging().getToken();
+  //     if (fcmToken) {
+  //       // user has a device token
+  //       await AsyncStorage.setItem('fcmToken', fcmToken);
+  //     }
+  //   }
+  // }
+
+  const requestPermission = async () => {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
+  }
+  // async requestPermission() {
+  //   try {
+  //     await firebase.messaging().requestPermission();
+  //     // User has authorised
+  //     this.getToken();
+  //   } catch (error) {
+  //     // User has rejected permissions
+  //     console.log('permission rejected');
+  //   }
+  // }
+
+  const createNotificationListeners = async () => {
+    // This listener triggered when notification has been received in foreground
+    notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      console.log(title);
+      console.log(body);
+      displayNotification(title, body);
+    });
+
+    // This listener triggered when app is in backgound and we click, tapped and opened notifiaction
+    notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      displayNotification(title, body);
+    });
+
+    // This listener triggered when app is closed and we click,tapped and opened notification 
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      displayNotification(title, body);
+    }
+  }
+  // async createNotificationListeners() {
+  //   // This listener triggered when notification has been received in foreground
+  //   this.notificationListener = firebase.notifications().onNotification((notification) => {
+  //     const { title, body } = notification;
+  //     this.displayNotification(title, body);
+  //   });
+
+  //   // This listener triggered when app is in backgound and we click, tapped and opened notifiaction
+  //   this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+  //     const { title, body } = notificationOpen.notification;
+  //     this.displayNotification(title, body);
+  //   });
+
+  //   // This listener triggered when app is closed and we click,tapped and opened notification 
+  //   const notificationOpen = await firebase.notifications().getInitialNotification();
+  //   if (notificationOpen) {
+  //     const { title, body } = notificationOpen.notification;
+  //     this.displayNotification(title, body);
+  //   }
+  // }
+
+
+  const displayNotification = (title, body) => {
+    // we display notification in alert box with title and body
+    Alert.alert(
+      title, body,
+      [
+        { text: 'Ok', onPress: () => console.log('ok pressed') },
+      ],
+      { cancelable: false },
+    );
+  }
+  // displayNotification(title, body) {
+  //   // we display notification in alert box with title and body
+  //   Alert.alert(
+  //     title, body,
+  //     [
+  //       { text: 'Ok', onPress: () => console.log('ok pressed') },
+  //     ],
+  //     { cancelable: false },
+  //   );
+  // }
+  /**
+   * 
+   */
+
+
+
+
+  if (loginState.isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
       </View>
     );
   }
   return (
     <PaperProvider theme={theme}>
-    <AuthContext.Provider value={authContext}>
-    <NavigationContainer theme={theme}>
-      { loginState.userToken !== null ? (
-        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
-          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
-          {/* <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+      <AuthContext.Provider value={authContext}>
+        <NavigationContainer theme={theme}>
+          {loginState.userToken !== null ? (
+            <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+              <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+              {/* <Drawer.Screen name="SupportScreen" component={SupportScreen} />
           <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
           <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} /> */}
-        </Drawer.Navigator>
-      )
-    :
-      <RootStackScreen/>
-    }
-    </NavigationContainer>
-    </AuthContext.Provider>
+            </Drawer.Navigator>
+          )
+            :
+            <RootStackScreen />
+          }
+        </NavigationContainer>
+      </AuthContext.Provider>
     </PaperProvider>
   );
 }
